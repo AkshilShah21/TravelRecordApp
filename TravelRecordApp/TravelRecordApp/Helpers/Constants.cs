31d1048454d0632107023ec173fce9cc7c99e499
AkshilShah21 <114335339+AkshilShah21@users.noreply.github.com>
2022-09-25 16:05:44 +0530

    Update Constants.cs

--- TravelRecordApp/TravelRecordApp/Helpers/Constants.cs
+++ TravelRecordApp/TravelRecordApp/Helpers/Constants.cs
@@ -7,7 +7,7 @@ namespace TravelRecordApp.Helpers
     public class Constants
     {
         public const string VENUE_SEARCH = "https://api.foursquare.com/v3/places/nearby?ll={0}%2C{1}";
-        public const string key = "fsq3OjqhBtwNUeMbwJyy948zmQ5NnuTz0Aw8VRSD8EHpmws=";
+        public const string key = "fsq3OjqhBtwNUeMbwJyy9utuymQ5NnuTz0Aw8VRSD8EHpmws=";
     }
 }
 
